79026ef438932edea8480bee0e588d724349f3dd
Dimitar Pashev <dim.pashev@gmail.com>
2025-04-07 11:39:01 +0300

    added django-redis

--- src/home/settings.py
+++ src/home/settings.py
@@ -189,3 +189,18 @@ STORAGES = {
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REDIS_URL = config("REDIS_URL", cast=str, default="")
+
+### REDIS CACHE
+if REDIS_URL:
+    CACHES = {
+        "default": {
+            "BACKEND": "django_redis.cache.RedisCache",
+            "LOCATION": REDIS_URL,
+            "OPTIONS": {
+                "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            }
+        }
+    }
